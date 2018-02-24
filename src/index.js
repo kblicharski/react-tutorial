3535dd155e6e41a63e50a4e461054ad74c4cd370
Kevin Blicharski <blicharskikevin@gmail.com>
2018-02-24 10:03:18 -0600

    Modify Square to be a stateless, functional component

--- src/index.js
+++ src/index.js
@@ -6,29 +6,50 @@ import './index.css';
 // This component type stores no state of its own --
 // it receives values from its parent and notifies its parent
 // when interacted with.
-class Square extends React.Component {
 
-  render() {
+// class Square extends React.Component {
+//
+//   render() {
+//     return (
+//         // Note that we're passing a function
+//         // to onClick. If we didn't, it would
+//         // happen immediately instead of on
+//         // each button click.
+//       <button
+//         className="square"
+//         onClick={() => this.props.onClick()}>
+//         {this.props.value}
+//       </button>
+//         // Note that we are calling this.setState.
+//         // Whenever modifying the state of a component,
+//         // you should call this function so React can
+//         // rerender it and its descendants.
+//     );
+//   }
+//
+// }
+
+// The above example can be more succinctly expressed
+// as a "functional component". These are components that
+// only have a "render" method and no internal state.
+//
+// Note that we're binding the function reference "props.onClick"
+// to the button attribute "onClick". The same could have been
+// accomplished using {() => props.onClick()}
+function Square(props) {
     return (
-        // Note that we're passing a function
-        // to onClick. If we didn't, it would
-        // happen immediately instead of on
-        // each button click.
-      <button 
-        className="square"
-        onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-        // Note that we are calling this.setState.
-        // Whenever modifying the state of a component,
-        // you should call this function so React can
-        // rerender it and its descendants.
+        <button
+            className="square"
+            onClick={props.onClick}
+        >
+            {props.value}
+        </button>
     );
-  }
-
 }
 
+
 class Board extends React.Component {
+
     constructor(props) {
         // In JS, when subclassing we always have to
         // explicitly call super.
@@ -41,7 +62,7 @@ class Board extends React.Component {
     // Note that this function is pure, because it enforces
     // immutability in our state. We copy squares before modifying
     // it, and then reset the entire container.
-    // 
+    //
     // Immutability is important because:
     //      1. It increases component and application performance.
     //      2. It lets us maintain a history of changes. With this,
