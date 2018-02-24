5f9ed7abb87f73769630be17c1d63a09112b6be3
Kevin Blicharski <blicharskikevin@gmail.com>
2018-02-24 10:10:46 -0600

    Modify the game to let both players play

--- src/index.js
+++ src/index.js
@@ -40,9 +40,9 @@ function Square(props) {
   return (
     <button
       className="square"
-      onClick={ props.onClick }
+      onClick={props.onClick}
     >
-      { props.value }
+      {props.value}
     </button>
   );
 }
@@ -54,8 +54,8 @@ class Board extends React.Component {
     // explicitly call super.
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
-
+      squares: Array(9).fill(null),
+      xIsNext: true
     };
   }
 
@@ -74,39 +74,42 @@ class Board extends React.Component {
   //      4. Pure components allow React to easily re-render.
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
   }
 
   renderSquare(i) {
     return (
       <Square
-        value={ this.state.squares[i] }
-        onClick={ () => this.handleClick(i) }
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
       />
     );
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
-        <div className="status">{ status }</div>
+        <div className="status">{status}</div>
         <div className="board-row">
-          { this.renderSquare(0) }
-          { this.renderSquare(1) }
-          { this.renderSquare(2) }
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
         <div className="board-row">
-          { this.renderSquare(3) }
-          { this.renderSquare(4) }
-          { this.renderSquare(5) }
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
         </div>
         <div className="board-row">
-          { this.renderSquare(6) }
-          { this.renderSquare(7) }
-          { this.renderSquare(8) }
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
         </div>
       </div>
     );
@@ -121,8 +124,8 @@ class Game extends React.Component {
           <Board/>
         </div>
         <div className="game-info">
-          <div>{ /* status */ }</div>
-          <ol>{ /* TODO */ }</ol>
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
         </div>
       </div>
     );
